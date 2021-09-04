a48fee42f45bbad9bc14b710b231992365c9e7a3
florin-botea-1693 <florinbotea1693@gmail.com>
2021-09-04 09:33:42 +0300

    gg

--- src/PhpTemplate.php
+++ src/PhpTemplate.php
@@ -6,6 +6,15 @@ class PhpTemplates
 {
     // requestName => [requestName => filetime, ...other components]
     private static $dependencies = null;
+    private static $timestamps = []; // cache file $timestamps
+    
+    public static function getUpdatedAt(string $file)
+    {
+        if (!isset(self::$timestamps[$file])) {
+            self::$timestamps[$file] = filetime($file);
+        }
+        return self::$timestamps[$file];
+    }
     
     private $options = [
         'prefix' => '@',
@@ -36,18 +45,49 @@ class PhpTemplates
     public function load(string $rfilepath, array $data = [], array $slots = [], array $options = [])
     {
         $this->requestName = preg_replace('(\.template|\.php)', '', $rfilepath);
-        $this->rfilepath = $this->requestName.'.template.php';
-        $this->file = $this->options->src_path.$this->rfilepath;
-        $this->timestamp = fileatime($this->file);
-        $this->slotsHash = $this->getSlotsHash();
-        $this->destFile = $this->getDestFilePath(); // based on req file, timestamp, slotsHash
         
-        $hasChanged = $this->syncDependencies($this->requestName);
-        if (!$hasChanged && $this->destFile && file_exists($this->destFile)) {
-            return require($this->destFile);
+        $this->srcFile = $this->getSrcFile();
+        // $this->timestamp = fileatime($this->file);
+        // $this->slotsHash = $this->getSlotsHash();
+        $this->destFile = $this->getDestFile(); // based on req file, timestamp, slotsHash
+        
+        $hasChanged = $this->options->track_changes && $this->syncDependencies($this->requestName);
+        if (!$hasChanged && file_exists($this->destFile) {
+            return require($this->destFile;
         }
         
-        
+        new parser($cfg)->parse(file) intoarce str html
+        replaces urile salvate pe o statica
+        cand ajung la un comp, inlocuiesc nodul cu chemare funcie cf reqname.i
+        ii trec ca sloturi nodurile si intorc cu load() optiune sa imi intoarca string si am ajuns aici
+        save pe statica nume f => declarare function
+        la fel si cand dau de sloturi daca sunt, daca nu, delete nodurile
+        la final, trb doar sa fac replaceuri si output si sa curat staticele
+    }
+    
+    private function getSrcFile()
+    {
+        if (!$this->srcFile) {
+            $f = $this->options->src_path.$this->rfilepath;
+            $this->srcFile = $f.$this->requestName.'.template.php';
+        }
+        return $this->srcFile;
+    }
+    
+    // based on this output, we decide if to recompile template
+    private function getDestFile()
+    {
+        if (!$this->destFile) {
+            $f = str_replace('/', '_', $this->requestName);
+            if ($this->options->track_changes) {
+                $f .= '-'.self::getUpdatedAt($this->getSrcFile());
+            }
+            if ($slotsHash = $this->getSlotsHash()) {
+                $f .= '-'.$slotsHash;
+            }
+            $this->destFile = $f;
+        }
+        return $this->destFile;
     }
     
     private function syncDependencies(string $reqName): bool
@@ -86,9 +126,6 @@ class PhpTemplates
         }
         return substr(md5($string, true), 0, 12);
     }
-<<<<<<< HEAD
-    ggg
-=======
     
->>>>>>> branch 'dev' of https://github.com/florin-botea-1693/php-templates
+    
 }
