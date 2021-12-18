22365b0a4652120b9f3711b3afe176c18a41b510
florin-botea-1693 <florinbotea1693@gmail.com>
2021-12-18 16:39:39 +0200

    rh

---
+++ tests/parser.php
@@ -0,0 +1,61 @@
+<?php
+
+require('../autoload.php');
+
+use DomDocument\PhpTemplates\Facades\Template;
+use DomDocument\PhpTemplates\Facades\Config;
+use DomDocument\PhpTemplates\Document;
+use DomDocument\PhpTemplates\Parser;
+use IvoPetkov\HTML5DOMDocument;
+
+Config::set('aliased', [
+    'x-form-group' => 'components/form-group',
+    'x-input-group' => 'components/input-group',
+    'x-card' => 'components/card',
+    'x-helper' => 'components/helper',
+]);
+
+Config::set('src_path', './');
+Config::set('dest_path', './results/');
+
+$files = scandir('./cases');
+$files = array_diff($files, ['..', './']);
+//dd(Config::all());
+foreach($files as $f) {
+    $file = './cases/'.$f;
+    $content = file_get_contents($file);
+    $cases = explode('-----', $content);
+    $test = '';
+    $expected = [];
+    foreach ($cases as $case) {
+        list($t, $exp) = explode('=====', $case);
+        $test .= $t.PHP_EOL.'-----'.PHP_EOL;
+        $expected[] = $exp;
+    }
+    $rfilepath = str_replace('.template.php', '', $file);
+    $doc = new Document($rfilepath);
+    $parser = new Parser($doc, $rfilepath);
+    $dom = new HTML5DOMDocument;
+    $dom->loadHtml($test);
+    $parser->parse($dom);
+    $dest = './results/'.str_replace('.template', '', $f);
+    $doc->save($dest);
+    ob_start();
+    $data = [];
+    include $dest;
+    $results = ob_get_clean();//dd($results);
+    $results = str_replace(["<!DOCTYPE html>\n<html>\n<body>"], '', $results);//dd($results);
+    $results = explode('-----',$results);
+    foreach ($results as $i => $result) {
+        if (empty($expected[$i])) {
+            continue;
+        }
+        $result = preg_replace('/[\n\r\t\s]*/', '', $result);
+        $_expected = preg_replace('/[\n\r\t\s]*/', '', $expected[$i]);
+        if ($result === $_expected) {
+            echo $f."[$i] passed \n";
+        } else {
+            echo $f."[$i] failed \n"; die();
+        }
+    }
+}
