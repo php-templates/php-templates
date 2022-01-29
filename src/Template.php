5c0caf18e3db5db303a0e87100d67369dba96ad9
florin-botea-1693 <59554192+florin-botea-1693@users.noreply.github.com>
2022-01-29 15:10:53 +0200

    fix

--- src/Template.php
+++ src/Template.php
@@ -15,7 +15,7 @@ class Template
         $start_time = microtime(true);
         $template = $this->get($rfilepath);
         $template->render($data);
-        d(microtime(true) - $start_time);
+        print_r('<br>'.(microtime(true) - $start_time));
     }
 
     public function get(string $rfilepath, array $data = [])
@@ -26,18 +26,18 @@ class Template
             $requestName = preg_replace('(\.template|\.php)', '', $rfilepath);
             $doc = new Document($requestName);
             if ($path = $doc->exists()) {
-                
+
             } else {
                 $parser = new Parser($doc, $rfilepath);
                 $parser->parse();
                 $path = $doc->save();
             }
-            
+
             require_once($path);
             return Parsed::template($requestName, $data);
         }
     }
-    
+
     public function raw(\Closure $cb, $data = [])
     {
         return Parsed::raw(null, $cb, $data);
