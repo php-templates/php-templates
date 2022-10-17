ffcfe88d5402e16b96e3034f22c6faaad48bb1c4
Florin Botea <florinbotea777@gmail.com>
2022-10-17 13:56:31 +0300

    config made

--- src/Config.php
+++ src/Config.php
@@ -2,25 +2,56 @@
 
 namespace PhpTemplates;
 
+use Exception;
 use PhpTemplates\Dom\DomNode;
-use PhpTemplates\Dom\PhpNode;
-use PhpTemplates\Dom\PhpNodeValAttr;
-use PhpTemplates\Dom\PhpNodeBindAttr;
+use PhpTemplates\Dom\PhpNodes\PhpNode;
 
 class Config
 {
+    /**
+     * Parent config, if any
+     *
+     * @var self
+     */
     private $parent;
+
+    /**
+     * Child configs
+     *
+     * @var array
+     */
     private $childs = [];
-    
+
+    /**
+     * Config unique identifier key
+     *
+     * @var string
+     */
     private $name;
 
+    /**
+     * Where to search for the given template relative path (this path + relative path => absolute file path)
+     *
+     * @var array
+     */
     private $srcPath;
+
+    /**
+     * key => value array containing alias => relative_path of templates
+     *
+     * @var array
+     */
     public $aliased = [];
+
+    /**
+     * Config defined directives
+     *
+     * @var array
+     */
     public $directives = [];
-    
-    private $prefix = 'p-'; // todo: find and remove
-   
-    public function __construct($name, $srcPath, self $parent = null) {
+
+    public function __construct($name, $srcPath, self $parent = null)
+    {
         $this->parent = $parent;
         $this->name = $name;
         $this->srcPath = (array) $srcPath;
@@ -28,102 +59,44 @@ class Config
             $this->addDefaultDirectives();
         }
     }
-    
-    public function subconfig($name, $srcPath) 
-    {// todo: ensure existing is taken
+
+    /**
+     * Create a subconfig derived from actual config, meaning it will fallback on this parent if template, directive, alias not found
+     *
+     * @param string $name
+     * @param string $srcPath
+     * @return self
+     */
+    public function subconfig(string $name, string $srcPath): self
+    {
+        if ($this->find($name)) {
+            throw new Exception("A config with '$name' key already exists");
+        }
+
         $cfg = new Config($name, $srcPath, $this);
         $this->childs[] = $cfg;
-  
+
         return $this;
     }
-    
-    public function getParent() {
-        return $this->parent;
-    }
-    
-    public function getRoot() {
-        $cfg = $this;
-        while ($this->getParent()) {
-            $cfg = $this->getParent();
-        }
-        
-        return $cfg;
-    }
 
-    public function setDirective(string $key, \Closure $callable): void
-    {
-        $reserved = ['raw', 'bind', 'if', 'elseif', 'else', 'for', 'foreach'];
-        
-        if (in_array($key, $reserved)) {
-            throw new \Exception("System directive '$key' cannot be overriden");
-        }
-        
-        $this->directives[$key] = $callable;
-    }
-    
-    public function setAlias($key, string $component = ''): void
-    {
-        if (!is_array($key)) {
-            $aliased = [$key => $component];
-        } else {
-            $aliased = $key;
-        }
-        $this->aliased = array_merge($this->aliased, $aliased);
-    }
-    
-    public function setSrcPath($val)
-    {
-        $this->srcPath = $val;
-    }
-    
-    public function setName(string $name)
-    {
-        $this->name = $name;
-    }
-    
-    public function getPath() 
-    {
-        return $this->srcPath;
-    }
-    public function addPath(string $path) 
-    {
-        $this->srcPath[] = $path;
-    }
-    
-    public function getName() 
-    {
-        return $this->name;
-    }
-    
-    public function getAliased(string $name) 
-    {
-        $cfg = $this;
-        do {
-            if (isset($cfg->aliased[$name])) {
-                return $cfg->aliased[$name];
-            }
-        }
-        while ($cfg = $cfg->getParent());
-    }
-    
-    public function getDirective(string $name) 
-    {
-        $cfg = $this;
-        do {
-            if (isset($cfg->directives[$name])) {
-                return $cfg->directives[$name];
-            }
-        }
-        while ($cfg = $cfg->getParent());
-    }
-    
-    
-    public function isDefault() 
+    /**
+     * Check if config is default
+     *
+     * @return boolean
+     */
+    public function isDefault(): bool
     {
         return !$this->parent;
     }
-    
-    public function find(string $cfgkey) {
+
+    /**
+     * Find config by key in subconfigs, or itself if matched by key
+     *
+     * @param string $cfgkey
+     * @return self
+     */
+    public function find(string $cfgkey): ?self
+    {
         if ($this->name == $cfgkey) {
             return $this;
         }
@@ -133,43 +106,127 @@ class Config
             }
         }
     }
-    
-    private function addDefaultDirectives() 
+
+    private function addDefaultDirectives()
     {
         $cfg = $this;
         $controlStructures = ['if', 'elseif', 'else', 'for', 'foreach'];
-        
+
         foreach ($controlStructures as $statement) {
-            $this->directives[$statement] = function(DomNode $node, string $args) use ($statement) {
+            $this->directives[$statement] = function (DomNode $node, string $args) use ($statement) {
                 if (in_array($statement, ['elseif', 'else'])) {
-                    if (!$node->prevSibling || !in_array(str_replace('#php-', '', $node->prevSibling->nodeName), ['if', 'elseif'])) {//$node->parentNode->parentNode->d();dd($node->parentNode->parentNode->debug());
+                    if (!$node->prevSibling || !in_array(str_replace('#php-', '', $node->prevSibling->nodeName), ['if', 'elseif'])) { //$node->parentNode->parentNode->d();dd($node->parentNode->parentNode->debug());
                         throw new InvalidNodeException("Unespected control structure '$statement'", $node);
                     }
                 }
                 $phpnode = new PhpNode($statement, $args);
                 $phpnode->indentStart = $node->indentStart;
                 $phpnode->indentEnd = $node->indentEnd;
-   
+
                 $node->parentNode->insertBefore($phpnode, $node);
                 $phpnode->appendChild($node->detach());
             };
         }
-        
-        $cfg->setDirective('checked', function(DomNode $node, string $val) {
+
+        $cfg->setDirective('checked', function (DomNode $node, string $val) {
             $node->addAttribute('p-raw', $val . ' ? "checked" : ""');
         });
-        
-        $cfg->setDirective('selected', function(DomNode $node, string $val) {
+
+        $cfg->setDirective('selected', function (DomNode $node, string $val) {
             $node->addAttribute('p-raw', $val . ' ? "selected=\"selected\"" : ""');
         });
-        
-        $cfg->setDirective('disabled', function(DomNode $node, string $val) {
+
+        $cfg->setDirective('disabled', function (DomNode $node, string $val) {
             $node->addAttribute('p-raw', $val . ' ? "disabled" : ""');
         });
     }
-    
-    public function __dhdhdget($prop)
+
+    // =================================================== //
+    // ===================== GETTERS ===================== //
+    // =================================================== //
+
+    public function getParent()
     {
-        return isset($this->{$prop}) ? $this->{$prop} : null;
+        return $this->parent;
+    }
+
+    public function getRoot()
+    {
+        $cfg = $this;
+        while ($this->getParent()) {
+            $cfg = $this->getParent();
+        }
+
+        return $cfg;
+    }
+
+    public function getPath()
+    {
+        return $this->srcPath;
+    }
+    public function addPath(string $path)
+    {
+        $this->srcPath[] = $path;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getAliased(string $name)
+    {
+        $cfg = $this;
+        do {
+            if (isset($cfg->aliased[$name])) {
+                return $cfg->aliased[$name];
+            }
+        } while ($cfg = $cfg->getParent());
+    }
+
+    public function getDirective(string $name)
+    {
+        $cfg = $this;
+        do {
+            if (isset($cfg->directives[$name])) {
+                return $cfg->directives[$name];
+            }
+        } while ($cfg = $cfg->getParent());
+    }
+
+
+    // =================================================== //
+    // ===================== GETTERS ===================== //
+    // =================================================== //
+
+    public function setDirective(string $key, \Closure $callable): void
+    {
+        $reserved = ['raw', 'bind', 'if', 'elseif', 'else', 'for', 'foreach'];
+
+        if (in_array($key, $reserved)) {
+            throw new \Exception("System directive '$key' cannot be overriden");
+        }
+
+        $this->directives[$key] = $callable;
+    }
+
+    public function setAlias($key, string $component = ''): void
+    {
+        if (!is_array($key)) {
+            $aliased = [$key => $component];
+        } else {
+            $aliased = $key;
+        }
+        $this->aliased = array_merge($this->aliased, $aliased);
+    }
+
+    public function setSrcPath($val)
+    {
+        $this->srcPath = $val;
+    }
+
+    public function setName(string $name)
+    {
+        $this->name = $name;
     }
 }
--- src/Dom/PhpNodes/PhpNode.php
+++ src/Dom/PhpNodes/PhpNode.php
@@ -1,44 +1,42 @@
 <?php
 
-namespace PhpTemplates\Dom;
+namespace PhpTemplates\Dom\PhpNodes;
 
-use PhpTemplates\Config;
-// todo, move this
+use PhpTemplates\Dom\DomNode;
+
 class PhpNode extends DomNode
-{ 
+{
     public function __toString()
     {
         // NODE START
         $indentNL = $this->getIndent();
         $return = $indentNL;
-        $isLoop = in_array($this->nodeName, ['for', 'foreach']); 
-        
+        $isLoop = in_array($this->nodeName, ['for', 'foreach']);
+
         if ($this->nodeName && $this->nodeName != '#text' && ($this->nodeValue || $this->nodeValue == '0')) {
             $expr = "{$this->nodeName} ({$this->nodeValue}) {";
-        } 
-        elseif($this->nodeName && $this->nodeName != '#text') {
+        } elseif ($this->nodeName && $this->nodeName != '#text') {
             $expr = $this->nodeName . ' {';
-        }
-        else {
+        } else {
             $expr = $this->nodeValue . ';';
         }
         $return .= '<?php ' . ($isLoop ? '$loopStart(); ' : '') . $expr . ' ?>';
-        
+
         // NODE CONTENT
         foreach ($this->childNodes as $cn) {
             $return .= $cn;
         }
-        
+
         // NODE END
         if ($this->nodeName && $this->nodeName != '#text') {
-            $return .= $indentNL . '<?php } ' .($isLoop ? '$loopEnd(); ' : ''). '?>';
+            $return .= $indentNL . '<?php } ' . ($isLoop ? '$loopEnd(); ' : '') . '?>';
         }
-        //d($return);
+
         return $return;
     }
-    
+
     public function getNodeName()
     {
-        return '#php-'.$this->nodeName;
+        return '#php-' . $this->nodeName;
     }
 }
